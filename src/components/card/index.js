ff947fd8f392f67136bf03c0f3acec8f1bf5cfa8
harley santos garzon <harley.santos.garzon@gmail.com>
2023-06-02 20:40:14 -0500

    refactor(component): improve card content

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,10 +1,26 @@
+import { getRelativeTime } from '../../helpers/internationalization.js'
+
 export function Card(props) {
-  const { title } = props
+  const { title, link, date, url } = props
+  let _date = ''
+  let _link = ''
+
+  if (date) {
+    const _dateFormated = getRelativeTime(new Date(date).getTime())
+    _date = `<time datetime="">${_dateFormated}</time>`
+  }
+
+  if (link || url) {
+    _link = `<a href="${link || url}" target="_blank">Visit post</a>`
+  }
 
   return `<div class="o-card">
     <div>
       ${title.rendered || title}
     </div>
-    <div></div>
+    <div>
+      ${_date}
+      ${_link}
+    </div>
   </div>`
 }
