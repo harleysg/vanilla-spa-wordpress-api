e740c242f97dc6fad9a7ac729b98bfad58519b43
harley santos garzon <harley.santos.garzon@gmail.com>
2023-06-02 20:54:21 -0500

    refactor(main): use hooks and include aside component

--- src/App.mjs
+++ src/App.mjs
@@ -1,13 +1,29 @@
+import { SearchDialogEventHook } from './helpers/searchDialogEvents.js';
+import { SearchHook } from './hooks/search.js';
+import { GetPostHook } from './hooks/getPost.js';
 import { Header } from './components/header/index.js';
 import { Main } from './components/main/index.js';
+import { SearchDialog } from './components/aside/index.js';
 
 const App = function ({root}) {
   render(root)
+    .then($root => {
+      hooks({root: $root})
+    })
 }
 
 function render(root) {
   root.appendChild(Header())
   root.appendChild(Main())
+  root.appendChild(SearchDialog())
+
+  return Promise.resolve(root)
+}
+
+function hooks({root}) {
+  SearchHook({root})
+  SearchDialogEventHook({root})
+  GetPostHook({root})
 }
 
 
