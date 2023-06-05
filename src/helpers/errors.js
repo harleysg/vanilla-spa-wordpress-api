d14951489dbf34c4331b270bbf419a53903e1d9f
harley santos garzon <harley.santos.garzon@gmail.com>
2023-06-05 14:41:51 -0500

    refactor(helpers): create ErrorFactory

--- src/helpers/errors.js
+++ src/helpers/errors.js
@@ -1,21 +1,53 @@
 import { API_FAIL_RESPONSE, GLOBALS_WP_API } from '../constants/index.js'
 
-export class ParamsError extends Error {
-  constructor(msn) {
-    const NAME = 'ParamsError'
-    const MESSAGE = `${NAME} | ${msn}`
-
+class ErrorFactory extends Error {
+  constructor(MESSAGE, NAME) {
     super(MESSAGE)
-    this.name = NAME
+    this.name = NAME || 'ErrorFactory'
     this.response = JSON.parse(JSON.stringify(API_FAIL_RESPONSE))
-    this.reporter()
+
+    this.validateGlobalThis()
+    this.reporter(MESSAGE)
   }
 
-  reporter() {
-    // TODO: Include a reporter
-    this.response.message = this.message
+  reporter(message) {
+    this.response.message = message
     this.response.type = this.name
+    this.response.id = new Date().getTime()
+
+    try {
+      // TODO: Include a external reporter
+      window.GLOBALS_WP_API.ERROR.push(this.response)
+    } catch (error) {
+      // TODO
+      console.warn({ 
+        ...this.response,
+        message: 'Fail reporter: '.concat(message)
+       })
+    }
 
     return this.response
+  }
+
+  validateGlobalThis() {
+    if (!window[GLOBALS_WP_API] && !window[GLOBALS_WP_API].ERROR) {
+      window[GLOBALS_WP_API] = {
+        ERROR: []
+      }
+    }
+  }
+}
+
+export class ContextError extends ErrorFactory {
+  constructor(msn) {
+    const _NAME = 'ContextError'
+    super(`${_NAME} | ${msn}`, _NAME)
+  }
+}
+
+export class ParamsError extends ErrorFactory {
+  constructor(msn) {
+    const _NAME = 'ParamsError'
+    super(`${_NAME} | ${msn}`, _NAME)
   }
 }
