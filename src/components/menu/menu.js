1ebf17bd769822a70906d1afe70b3b19cfc150ec
harley santos garzon <harley.santos.garzon@gmail.com>
2023-06-21 02:39:07 -0500

    refactor(component/menu): use new constants

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,14 +1,22 @@
+import { ROUTES } from '../../constants/index.js'
+
 export function Menu() {
   return Render()
 }
 
 function Render() {
   const $EL = document.createElement('nav')
-  const items = [{ text: 'Home', link: '#/'}, {text: 'contant', link: '#/contact' }]
+  const menuSorted = Object.entries(ROUTES)
+    .sort((a, b) => {
+      const [, prev_a] = a, [, prev_b] = b
+      if (prev_a.order < prev_b.order) return -1
+      if (prev_a.order > prev_b.order) return 1
+      return 0
+    })
 
   $EL.classList.add('o-header__nav')
-  $EL.innerHTML = items
-    .map(({text, link}) => `<a href="${link}"> ${text} </a>`)
+  $EL.innerHTML = menuSorted
+    .map(([, {link, name, anchor}]) => `<a href="${link}" sg-route="${anchor}"> ${name} </a>`)
     .join('')
 
   return $EL
