1ed6e71ca9b21abdcba5446fdfa8a3355742f041
harley santos garzon <harley.santos.garzon@gmail.com>
2023-07-06 17:49:11 -0500

    refactor(utils/*): create utils
    coverge browser and storage

---
+++ src/utils/browser/browserHistoryEngine.js
@@ -0,0 +1,38 @@
+const BrowserHistory = {
+  back: () => window.history.back(),
+
+  forward: () => window.history.forward(),
+
+  go: (n) => window.history.go(n),
+
+  setLocation: ({path, data, title} = ISetLocation) => {
+    const { history, location, document } = window
+    const { pathname, hash } = location
+
+    if (pathname + hash !== path) {
+      history.pushState(
+        data,
+        `${document.title} - ${title}`,
+        path
+      );
+    }
+  },
+
+  updateLocation: ({path, data, title} = ISetLocation) => {
+    const { history, location, document } = window
+
+    if (location.pathname !== path) {
+      history.replaceState(
+        data,
+        `${document.title} - ${title}`,
+        path
+      );
+    }
+  },
+
+  updateTitle: ({title} = {title: ''}) => {
+    window.document.title = title
+  }
+}
+
+export default BrowserHistory
